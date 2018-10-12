ef771163dad6adc01bf392d70afff774a18613fd
Salvador Martinez de Bartolome Izquierdo <salvador@scripps.edu>
2018-10-12 14:25:48 -0700

    latest version including renaming of files

--- src/main/java/edu/scripps/yates/ip2tomassive/MultiProjectUpload.java
+++ src/main/java/edu/scripps/yates/ip2tomassive/MultiProjectUpload.java
@@ -2,25 +2,41 @@ package edu.scripps.yates.ip2tomassive;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+
+import gnu.trove.map.hash.THashMap;
 
 public class MultiProjectUpload {
 	public static void main(String[] args) {
-		File propertiesFile = new File(args[0]);
+		final File propertiesFile = new File(args[0]);
 		File remotePathsFile = null;
 
 		if (args.length > 1) {
 			remotePathsFile = new File(args[1]);
 		}
-
-		MySftpProgressMonitor progressMonitor = new MySftpProgressMonitor(System.out);
+		final Map<String, String> translations = new THashMap<String, String>();
+		translations.put("X09", "normal1");
+		translations.put("X5709", "normal1");
+		translations.put("X83", "normal2");
+		translations.put("X5783", "normal2");
+		translations.put("X5750", "AD1");
+		translations.put("X5743", "AD2");
+		translations.put("X5248", "control3");
+		translations.put("X4870", "control4");
+		translations.put("X5763", "AD3");
+		translations.put("X5798", "AD4");
+		translations.put("X43", "AD2");
+		translations.put("X50", "AD1");
+		final MySftpProgressMonitor progressMonitor = new MySftpProgressMonitor(System.out);
 		try {
-			MultipleProjectIP2ToMassive m = new MultipleProjectIP2ToMassive(progressMonitor, propertiesFile,
-					remotePathsFile);
-			m.setSubmissionName("CFTR");
+			final MultipleProjectIP2ToMassive m = new MultipleProjectIP2ToMassive(progressMonitor, propertiesFile,
+					translations, remotePathsFile);
+			m.setSubmissionName("Surface Labelling");
+
 			m.transferDatasets();
 			System.out.println("PROGRAM FINISHED OK");
 			System.exit(0);
-		} catch (IOException e) {
+		} catch (final IOException e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 			System.exit(-1);
--- src/main/java/edu/scripps/yates/ip2tomassive/MultipleProjectIP2ToMassive.java
+++ src/main/java/edu/scripps/yates/ip2tomassive/MultipleProjectIP2ToMassive.java
@@ -30,10 +30,12 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 	protected static final String DATASET = "DATASET";
 	private final Map<String, Dataset> datasetsByName = new HashMap<String, Dataset>();
 	private String submissionName = "";
+	private final Map<String, String> keywordTranslations;
 
-	public MultipleProjectIP2ToMassive(MySftpProgressMonitor progressMonitor, File propertiesFile, File remotePathsFile)
-			throws IOException {
+	public MultipleProjectIP2ToMassive(MySftpProgressMonitor progressMonitor, File propertiesFile,
+			Map<String, String> keywordTranslations, File remotePathsFile) throws IOException {
 		super(progressMonitor, propertiesFile);
+		this.keywordTranslations = keywordTranslations;
 		if (remotePathsFile != null) {
 			processRemotePaths(remotePathsFile);
 		}
@@ -41,10 +43,13 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 
 	public void transferDatasets() {
 		long totalTransferredSize = 0l;
-		for (String datasetName : datasetsByName.keySet()) {
+		for (final String datasetName : datasetsByName.keySet()) {
 			log.info(datasetName + " transfer started");
-			totalTransferredSize += transferDataset(datasetName);
-			log.info(datasetName + " transfer finished");
+			final long transferDatasetSize = transferDataset(datasetName);
+			totalTransferredSize += transferDatasetSize;
+			log.info(datasetName + " (" + FileUtils.getDescriptiveSizeFromBytes(transferDatasetSize)
+					+ ") transfer finished. Total transfered so far: "
+					+ FileUtils.getDescriptiveSizeFromBytes(totalTransferredSize));
 
 		}
 		System.out.println(FileUtils.getDescriptiveSizeFromBytes(totalTransferredSize) + " transferred in "
@@ -57,13 +62,13 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 		if (dataset == null) {
 			throw new IllegalArgumentException(datasetName + " dataset not found");
 		}
-		Set<FileType> fileTypes = dataset.getPathsByFileType().keySet();
+		final Set<FileType> fileTypes = dataset.getPathsByFileType().keySet();
 		final Map<String, String> outputFileNamesByPath = dataset.getOutputFileNameByPath();
-		for (FileType fileType : fileTypes) {
+		for (final FileType fileType : fileTypes) {
 			final List<String> paths = dataset.getPathsByFileType().get(fileType);
-			for (String path : paths) {
+			for (final String path : paths) {
 				final String outputFileName = outputFileNamesByPath.get(path);
-				long transferredSize = transferFile(path, outputFileName, fileType, dataset);
+				final long transferredSize = transferFile(path, outputFileName, fileType, dataset);
 				totalTransferredSize += transferredSize;
 				System.out.println(FileUtils.getDescriptiveSizeFromBytes(totalTransferredSize) + " transferred in "
 						+ datasetName + " dataset so far");
@@ -76,7 +81,7 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 	}
 
 	private void processRemotePaths(File remotePathsFile) throws IOException {
-		List<String> readAllLines = Files.readAllLines(Paths.get(remotePathsFile.toURI()));
+		final List<String> readAllLines = Files.readAllLines(Paths.get(remotePathsFile.toURI()));
 		FileType currentFileType = null;
 		String currentDataSet = null;
 		for (String line : readAllLines) {
@@ -88,6 +93,13 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 					final String[] split = line.split("\t");
 					path = split[0];
 					fileName = split[1];
+					final String keywordToTranslate = getKeywordToTranslate(fileName);
+					if (keywordToTranslate != null) {
+						final String newName = fileName.replace(keywordToTranslate,
+								keywordTranslations.get(keywordToTranslate));
+						log.info("File named as " + fileName + " now is mapped to " + newName);
+						fileName = newName;
+					}
 				}
 				if (fileName == null) {
 					// it is because it is a wild card
@@ -105,6 +117,19 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 				}
 			}
 		}
+	}
+
+	private String getKeywordToTranslate(String fileName) {
+		for (final String keyword : keywordTranslations.keySet()) {
+			if (fileName.contains(keyword)) {
+				return keyword;
+			}
+		}
+		return null;
+	}
+
+	private Map<String, String> getKeywordTranslations() {
+		return keywordTranslations;
 	}
 
 	private boolean isPath(String line) {
@@ -126,12 +151,12 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 			try {
 				ftpMassive = loginToMassive();
 
-				if (this.submissionName != null && !"".equals(submissionName)) {
+				if (submissionName != null && !"".equals(submissionName)) {
 					folderPathToMassive = "/" + submissionName + "/" + folderPathToMassive;
 				}
 				log.info("Transferring file " + fullPathToIP2);
 				createFullPathInRemote(ftpMassive, folderPathToMassive);
-			} catch (Exception e) {
+			} catch (final Exception e) {
 				e.printStackTrace();
 				throw e;
 			} finally {
@@ -146,55 +171,62 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 				// path/to/files/*.raw
 				final String extension = FilenameUtils.getExtension(fullPathToIP2);
 
-				String pathToFolderInIP2 = FilenameUtils.getFullPath(fullPathToIP2);
+				final String pathToFolderInIP2 = FilenameUtils.getFullPath(fullPathToIP2);
 
-				List<LsEntry> ftpFilesInIP2 = FTPUtils.getFilesInFolderByExtension(sshIP2, pathToFolderInIP2,
+				final List<LsEntry> ftpFilesInIP2 = FTPUtils.getFilesInFolderByExtension(sshIP2, pathToFolderInIP2,
 						extension);
 
-				for (LsEntry ftpFileInIP2 : ftpFilesInIP2) {
+				for (final LsEntry ftpFileInIP2 : ftpFilesInIP2) {
 
 					try {
 						fullPathToIP2 = pathToFolderInIP2 + ftpFileInIP2.getFilename();
 						String fileName = FilenameUtils.getName(fullPathToIP2);
-
+						final String keywordToTranslate = getKeywordToTranslate(fileName);
+						if (keywordToTranslate != null) {
+							final String newName = fileName.replace(keywordToTranslate,
+									keywordTranslations.get(keywordToTranslate));
+							log.info("File named as " + fileName + " now is mapped to " + newName);
+							fileName = newName;
+						}
 						progressMonitor.setSuffix("(" + fileName + ") ");
 
-						long sizeInIP2 = ftpFileInIP2.getAttrs().getSize();
-						String fullPathToMassive = folderPathToMassive + "/" + fileName;
+						final long sizeInIP2 = ftpFileInIP2.getAttrs().getSize();
+						final String fullPathToMassive = folderPathToMassive + "/" + fileName;
 						long sizeInMassive = 0;
 						try {
 							ftpMassive = loginToMassive();
 							ftpMassive.setFileType(FTP.BINARY_FILE_TYPE);
 							sizeInMassive = FTPUtils.getSize(ftpMassive, fullPathToMassive);
-
-							if (sizeInMassive == sizeInIP2) {
-								log.info("File  '" + FilenameUtils.getName(fullPathToIP2) + "' found in MassIVE as '"
-										+ fullPathToMassive + "' with  "
-										+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2) + ". It will be skipped");
-								sizeTransferred += sizeInIP2;
-								continue;
-							} else if (sizeInMassive > sizeInIP2) {
-								log.info(
-										"File '" + FilenameUtils.getName(fullPathToIP2) + "' is bigger in MassIVE: IP2:"
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2) + " Massive:"
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive) + " diff: "
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive - sizeInIP2));
-							} else if (sizeInIP2 > sizeInMassive) {
-								log.info(
-										"File '" + FilenameUtils.getName(fullPathToIP2) + "' is bigger in MassIVE: IP2:"
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2) + " Massive:"
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive) + " diff: "
-												+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2 - sizeInMassive));
+							if (sizeInMassive > -1) {
+								if (sizeInMassive == sizeInIP2) {
+									log.info("File  '" + FilenameUtils.getName(fullPathToIP2)
+											+ "' found in MassIVE as '" + fullPathToMassive + "' with  "
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2)
+											+ ". It will be skipped");
+									sizeTransferred += sizeInIP2;
+									continue;
+								} else if (sizeInMassive > sizeInIP2) {
+									log.info("File '" + FilenameUtils.getName(fullPathToIP2)
+											+ "' is bigger in MassIVE: IP2:"
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2) + " Massive:"
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive) + " diff: "
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive - sizeInIP2));
+								} else if (sizeInIP2 > sizeInMassive) {
+									log.info("File '" + FilenameUtils.getName(fullPathToIP2)
+											+ "' is bigger in MassIVE: IP2:"
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2) + " Massive:"
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInMassive) + " diff: "
+											+ FileUtils.getDescriptiveSizeFromBytes(sizeInIP2 - sizeInMassive));
+								}
 							}
-
 							log.info("Output file in Massive: " + fullPathToMassive);
 
-							OutputStream outputStreamInMassive = ftpMassive.storeFileStream(fullPathToMassive);
+							final OutputStream outputStreamInMassive = ftpMassive.storeFileStream(fullPathToMassive);
 							FTPUtils.showServerReply(ftpMassive);
 							if (outputStreamInMassive == null) {
 								throw new IllegalArgumentException("Error trying to create output stream to Massive");
 							}
-							ChannelSftp sftpChannel = FTPUtils.openSFTPChannel(sshIP2);
+							final ChannelSftp sftpChannel = FTPUtils.openSFTPChannel(sshIP2);
 
 							sftpChannel.get(fullPathToIP2, outputStreamInMassive, progressMonitor);
 							sizeTransferred += sizeInIP2;
@@ -202,7 +234,7 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 							outputStreamInMassive.close();
 							log.info(
 									"Transfer of " + FileUtils.getDescriptiveSizeFromBytes(sizeTransferred) + " done.");
-							if (sizeTransferred != sizeInIP2) {
+							if (sizeTransferred != sizeInIP2 && sizeInIP2 > -1) {
 								log.warn(FileUtils.getDescriptiveSizeFromBytes(sizeTransferred - sizeInIP2));
 							}
 						} finally {
@@ -211,25 +243,25 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 							FTPUtils.showServerReply(ftpMassive);
 						}
 
-					} catch (IOException e) {
+					} catch (final IOException e) {
 						e.printStackTrace();
 						log.warn(e.getMessage());
 
-					} catch (SftpException e) {
+					} catch (final SftpException e) {
 						e.printStackTrace();
 						log.warn(e.getMessage());
 					}
 
 				}
 			} else {
-				String fileName = FilenameUtils.getName(outputFileName);
+				final String fileName = FilenameUtils.getName(outputFileName);
 				progressMonitor.setSuffix("(" + fileName + ") ");
 				try {
-					long sizeInIP2 = FTPUtils.getSize(sshIP2, fullPathToIP2);
+					final long sizeInIP2 = FTPUtils.getSize(sshIP2, fullPathToIP2);
 					long sizeInMassive = 0;
 					try {
 						ftpMassive = loginToMassive();
-						String fullPathToMassive = folderPathToMassive + "/" + fileName;
+						final String fullPathToMassive = folderPathToMassive + "/" + fileName;
 						sizeInMassive = FTPUtils.getSize(ftpMassive, fullPathToMassive);
 
 						if (sizeInIP2 <= sizeInMassive) {
@@ -244,13 +276,13 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 
 						final String outputFullPath = folderPathToMassive + "/" + fileName;
 						log.info("Output file in Massive: " + outputFullPath);
-						OutputStream outputStreamInMassive = ftpMassive.storeFileStream(outputFullPath);
+						final OutputStream outputStreamInMassive = ftpMassive.storeFileStream(outputFullPath);
 						FTPUtils.showServerReply(ftpMassive);
 						if (outputStreamInMassive == null) {
 							throw new IllegalArgumentException("Error trying to create output stream to Massive");
 						}
 
-						ChannelSftp sftpChannel = FTPUtils.openSFTPChannel(sshIP2);
+						final ChannelSftp sftpChannel = FTPUtils.openSFTPChannel(sshIP2);
 						sftpChannel.get(fullPathToIP2, outputStreamInMassive, progressMonitor);
 						sftpChannel.exit();
 						outputStreamInMassive.close();
@@ -261,27 +293,27 @@ public class MultipleProjectIP2ToMassive extends IP2ToMassive {
 						FTPUtils.showServerReply(ftpMassive);
 					}
 
-				} catch (IOException e) {
+				} catch (final IOException e) {
 					e.printStackTrace();
 					log.warn(e.getMessage());
-				} catch (JSchException e) {
+				} catch (final JSchException e) {
 					e.printStackTrace();
 					log.warn(e.getMessage());
-				} catch (SftpException e) {
+				} catch (final SftpException e) {
 					e.printStackTrace();
 					log.warn(e.getMessage());
 				}
 			}
-		} catch (SocketException e2) {
+		} catch (final SocketException e2) {
 			e2.printStackTrace();
 			log.warn(e2.getMessage());
-		} catch (IOException e2) {
+		} catch (final IOException e2) {
 			e2.printStackTrace();
 			log.warn(e2.getMessage());
-		} catch (JSchException e1) {
+		} catch (final JSchException e1) {
 			e1.printStackTrace();
 			log.warn(e1.getMessage());
-		} catch (SftpException e1) {
+		} catch (final SftpException e1) {
 			e1.printStackTrace();
 			log.warn(e1.getMessage());
 		} finally {
